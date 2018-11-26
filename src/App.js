505e168bdb26578c9efa10fe4f464e2592678905
RomariosGoIT <romantgt@gmail.com>
2018-11-26 13:48:52 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Creact new App </h1>
+        Weather App
+      </div>
+    );
+  }
+}
+
+export default App;
